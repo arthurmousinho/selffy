455ade1faa94da80ec60a2f4ed06cbbbe9e41ddd
arthurmousinho <arthursilvamo296@gmail.com>
2024-10-30 14:21:29 -0300

    🧪 test: adds count-projects-by-owner-id test

---
+++ api/src/application/use-cases/project/count-projects-by-owner-id/count-projects-by-owner-id.spec.ts
@@ -0,0 +1,38 @@
+import { CountProjectsByOwnerIdUseCase } from "./count-projects-by-owner-id.usecase";
+import { ProjectRepository } from "@application/repositories/project.repository";
+
+describe('CountProjectsByOwnerIdUseCase', () => {
+    let countProjectsByOwnerIdUseCase: CountProjectsByOwnerIdUseCase;
+    let projectRepository: ProjectRepository;
+
+    beforeEach(() => {
+        projectRepository = {
+            countByOwnerId: jest.fn(),
+        } as any;
+
+        countProjectsByOwnerIdUseCase = new CountProjectsByOwnerIdUseCase(projectRepository);
+    });
+
+    it('should return the correct project count for the given ownerId', async () => {
+        const ownerId = 'owner-id';
+        const expectedCount = 5;
+
+        (projectRepository.countByOwnerId as jest.Mock).mockResolvedValue(expectedCount);
+        const count = await countProjectsByOwnerIdUseCase.execute(ownerId);
+
+        expect(count).toBe(expectedCount);
+        expect(projectRepository.countByOwnerId).toHaveBeenCalledWith(ownerId);
+    });
+
+    it('should return 0 if no projects are found for the given ownerId', async () => {
+        const ownerId = 'owner-id';
+        const expectedCount = 0;
+
+        (projectRepository.countByOwnerId as jest.Mock).mockResolvedValue(expectedCount);
+        const count = await countProjectsByOwnerIdUseCase.execute(ownerId);
+
+        expect(count).toBe(expectedCount);
+        expect(projectRepository.countByOwnerId).toHaveBeenCalledWith(ownerId);
+    });
+
+});
