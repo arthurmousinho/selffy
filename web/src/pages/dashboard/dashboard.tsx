f0f8e5f5637c440db7db987f7170b043e49512f5
arthurmousinho <arthursilvamo296@gmail.com>
2024-11-19 10:00:02 -0300

    🗑️ remove: drops in progress project card component

--- web/src/pages/dashboard/dashboard.tsx
+++ web/src/pages/dashboard/dashboard.tsx
@@ -3,7 +3,6 @@ import { ActiveProjectsCard } from "@/components/dashboard/active-projects-card"
 import { TotalRevenueCard } from "@/components/dashboard/total-revenue-card";
 import { ProjectRankingCard } from "@/components/dashboard/project-ranking-card";
 import { WeekProductivityCard } from "@/components/dashboard/week-productivity-card";
-import { InProgressProjectCard } from "@/components/dashboard/in-progress-project-card";
 import { getUserDashboard } from "@/hooks/use-user";
 
 export function Dashboard() {
@@ -29,40 +28,6 @@ export function Dashboard() {
             <section className="grid grid-cols-2 gap-4">
                 <ProjectRankingCard ranking={dashboard?.projectRanking || []} />
                 <WeekProductivityCard data={dashboard?.weekProductivity || {} as any} />
-            </section>
-            <section>
-                <div className="grid grid-cols-3 gap-4">
-                    <InProgressProjectCard
-                        title="MyChaty"
-                        icon="💬"
-                        color="#86efac"
-                    />
-                    <InProgressProjectCard
-                        title="Better"
-                        icon="🕹️"
-                        color="#fca5a5"
-                    />
-                    <InProgressProjectCard
-                        title="Maple"
-                        icon="✅"
-                        color="#fdba74"
-                    />
-                    <InProgressProjectCard
-                        title="DevBooks"
-                        icon="📚"
-                        color="#67e8f9"
-                    />
-                    <InProgressProjectCard
-                        title="ExamChecker"
-                        icon="💊"
-                        color="#f9a8d4"
-                    />
-                    <InProgressProjectCard
-                        title="Boleto Facil"
-                        icon="🤑"
-                        color="#fca5a5"
-                    />
-                </div>
             </section>
         </main>
     )
